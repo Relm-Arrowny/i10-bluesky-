97b96a3c75682ca6b5bb3bb6ddbb1a13eba124df
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-01-08 16:11:31 +0000

    added test and fixes bugs

--- src/i10_bluesky/plans/align_slits.py
+++ src/i10_bluesky/plans/align_slits.py
@@ -1,6 +1,6 @@
 import math
 
-from bluesky.plan_stubs import mv, wait
+from bluesky.plan_stubs import abs_set, mv, wait
 from dodal.beamlines.i10 import (
     diffractometer,
     rasor_femto_pa_scaler_det,
@@ -24,8 +24,9 @@ def align_s5s6(
     s_stage = simple_stage()
     slit = slits()
     group_wait = "diff group A"
-    yield from mv(diff.tth, 0, diff.th, 0, group=group_wait)  # type: ignore  # See: https://github.com/bluesky/bluesky/issues/1809
-    yield from mv(s_stage.y, -3, group=group_wait)  # type: ignore
+    yield from abs_set(diff.tth, 0, group=group_wait)
+    yield from abs_set(diff.th, 0, group=group_wait)  # type: ignore  # See: https://github.com/bluesky/bluesky/issues/1809
+    yield from abs_set(s_stage.y, -3, group=group_wait)  # type: ignore
     yield from wait(group=group_wait)
 
     yield from align_slit(
@@ -63,10 +64,11 @@ def align_slit(
     centre_type: PeakPosition = PeakPosition.COM,
 ):
     group_wait = "slits group"
-    yield from mv(slit.x_gap, x_scan_size, slit.y_gap, y_open_size, group=group_wait)  # type: ignore
+    yield from abs_set(slit.x_gap, x_scan_size, group=group_wait)
+    yield from abs_set(slit.y_gap, y_open_size, group=group_wait)
+    yield from wait(group=group_wait)
     yield from mv(slit.y_centre, y_cen, group=group_wait)  # type: ignore
     start_pos, end_pos, num = slit_cal_range_num(x_cen, x_range, x_scan_size)
-    yield from wait(group=group_wait)
     yield from step_scan_and_move_cen(
         det=det,
         motor=slit.x_centre,
@@ -78,9 +80,10 @@ def align_slit(
         loc=centre_type,
     )
 
-    yield from mv(slit.y_gap, y_scan_size, slit.x_gap, x_open_size, group=group_wait)  # type: ignore
-    start_pos, end_pos, num = slit_cal_range_num(y_cen, y_range, y_scan_size)
+    yield from abs_set(slit.y_gap, y_scan_size, group=group_wait)
+    yield from abs_set(slit.x_gap, x_open_size, group=group_wait)
     yield from wait(group=group_wait)
+    start_pos, end_pos, num = slit_cal_range_num(y_cen, y_range, y_scan_size)
     yield from step_scan_and_move_cen(
         det=det,
         motor=slit.y_centre,
@@ -90,12 +93,13 @@ def align_slit(
         det_name=det_name,
         loc=centre_type,
     )
-
-    yield from mv(slit.x_gap, x_final_size, slit.y_gap, y_final_size)  # type: ignore
+    yield from abs_set(slit.x_gap, x_final_size, group=group_wait)
+    yield from abs_set(slit.y_gap, y_final_size, group=group_wait)
+    yield from wait(group=group_wait)
 
 
 def slit_cal_range_num(cen, range, size) -> tuple[float, float, int]:
     start_pos = cen - range
     end_pos = cen + range
-    num = math.ceil(range * 4 / size)
+    num = math.ceil(abs(range * 4.0 / size))
     return start_pos, end_pos, num
---
+++ tests/plans/test_align_slits.py
@@ -0,0 +1,144 @@
+from collections import defaultdict
+from unittest.mock import ANY, MagicMock, patch
+
+import pytest
+from bluesky.simulators import RunEngineSimulator, assert_message_and_return_remaining
+from dodal.beamlines.i10 import (
+    diffractometer,
+    rasor_femto_pa_scaler_det,
+    simple_stage,
+    slits,
+)
+
+from i10_bluesky.plans.align_slits import align_s5s6, align_slit, slit_cal_range_num
+
+docs = defaultdict(list)
+
+
+def capture_emitted(name, doc):
+    docs[name].append(doc)
+
+
+@pytest.mark.parametrize(
+    "test_input, expected_output",
+    [
+        (
+            [5, -5, 0.1],
+            (10, 0, 200),
+        ),
+        (
+            [50.6, 55.4, 15],
+            (-4.8, 106, 15),
+        ),
+        (
+            [
+                -1.1,
+                -2.5,
+                31,
+            ],
+            (1.4, -3.6, 1),
+        ),
+    ],
+)
+def test_slit_cal_range_num(test_input, expected_output):
+    assert slit_cal_range_num(*test_input) == pytest.approx(expected_output, 0.01)
+
+
+@patch(
+    "i10_bluesky.plans.align_slits.align_slit",
+)
+def test_align_s5s6(mock_align_slit: MagicMock):
+    sim = RunEngineSimulator()
+    msgs = sim.simulate_plan(align_s5s6())
+    mock_align_slit.assert_called_once()
+    msgs = check_msg_set(msgs=msgs, obj=diffractometer().tth, value=0)
+    msgs = check_msg_set(msgs=msgs, obj=diffractometer().th, value=0)
+    msgs = check_msg_set(msgs=msgs, obj=simple_stage().y, value=-3)
+    msgs = check_msg_wait(msgs=msgs, wait_group="diff group A")
+    assert len(msgs) == 1
+
+
+@patch("i10_bluesky.plans.align_slits.slit_cal_range_num", return_value=[1, 1, 1])
+@patch(
+    "i10_bluesky.plans.align_slits.step_scan_and_move_cen",
+)
+@pytest.mark.parametrize(
+    """x_scan_size, x_final_size, x_open_size, y_scan_size, y_final_size,
+      y_open_size, x_range, x_cen, y_range, y_cen""",
+    [
+        (0, 1, 2, 3.4, 5, 6, 7, 8, 9, 10),
+    ],
+)
+def test_align_slit(
+    mock_step_scan: MagicMock,
+    mock_cal_range: MagicMock,
+    x_scan_size,
+    x_final_size,
+    x_open_size,
+    y_scan_size,
+    y_final_size,
+    y_open_size,
+    x_range,
+    x_cen,
+    y_range,
+    y_cen,
+):
+    slit = slits().s5
+    det = rasor_femto_pa_scaler_det()
+    sim = RunEngineSimulator()
+    msgs = sim.simulate_plan(
+        align_slit(
+            det,
+            slit,
+            x_scan_size,
+            x_final_size,
+            x_open_size,
+            y_scan_size,
+            y_final_size,
+            y_open_size,
+            x_range,
+            x_cen,
+            y_range,
+            y_cen,
+        )
+    )
+    msgs = check_msg_set(msgs=msgs, obj=slit.x_gap, value=x_scan_size)
+    msgs = check_msg_set(msgs=msgs, obj=slit.y_gap, value=y_open_size)
+    msgs = check_msg_wait(msgs=msgs, wait_group="slits group")
+    msgs = check_msg_set(msgs=msgs, obj=slit.y_centre, value=y_cen)
+    msgs = check_mv_wait(msgs=msgs, wait_group=ANY)
+    msgs = check_msg_set(msgs=msgs, obj=slit.y_gap, value=y_scan_size)
+
+    msgs = check_msg_set(msgs=msgs, obj=slit.x_gap, value=x_open_size)
+    msgs = check_msg_wait(msgs=msgs, wait_group="slits group")
+    msgs = check_msg_set(msgs=msgs, obj=slit.x_gap, value=x_final_size)
+    msgs = check_msg_set(msgs=msgs, obj=slit.y_gap, value=y_final_size)
+    msgs = check_msg_wait(msgs=msgs, wait_group="slits group")
+    assert len(msgs) == 1
+    assert mock_step_scan.call_count == 2
+    assert mock_cal_range.call_count == 2
+
+
+def check_msg_set(msgs, obj, value):
+    return assert_message_and_return_remaining(
+        msgs,
+        lambda msg: msg.command == "set" and msg.obj is obj and msg.args[0] == value,
+    )
+
+
+def check_msg_wait(msgs, wait_group):
+    return assert_message_and_return_remaining(
+        msgs,
+        lambda msg: msg.command == "wait"
+        and msg.obj is None
+        and msg.kwargs == {"group": wait_group, "move_on": False, "timeout": None},
+    )
+
+
+def check_mv_wait(msgs, wait_group):
+    return assert_message_and_return_remaining(
+        msgs,
+        lambda msg: msg.command == "wait"
+        and msg.obj is None
+        and msg.kwargs == {"group": wait_group},
+    )
