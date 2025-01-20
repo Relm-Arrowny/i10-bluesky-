3992c091976e6b660d0531779ecec9a3a33d9e15
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-01-20 15:41:49 +0000

    add missing test

--- tests/plans/test_centre_direct_beam.py
+++ tests/plans/test_centre_direct_beam.py
@@ -5,7 +5,7 @@ from bluesky.run_engine import RunEngine
 from bluesky.simulators import RunEngineSimulator
 from dodal.beamlines.i10 import diffractometer
 
-from i10_bluesky.plans import centre_alpha, centre_tth
+from i10_bluesky.plans import centre_alpha, centre_det_angles, centre_tth
 
 from ..helper_functions import check_msg_set, check_mv_wait
 
@@ -40,3 +40,13 @@ async def test_centre_alpha(
     fake_step_scan_and_move_cen.assert_called_once_with(
         det=fake_detector, motor=diffractometer().alpha, start=-0.8, end=0.8, num=21
     )
+
+
+@patch("i10_bluesky.plans.centre_tth,")
+@patch("i10_bluesky.plans.centre_alpha")
+async def test_centre_det_angles(
+    centre_tth: Mock, centre_alpha: Mock, RE: RunEngine, fake_detector
+):
+    RE(centre_det_angles(det=fake_detector))
+    centre_tth.assert_called_once()
+    centre_alpha.assert_called_once()
