0627c5d600ba7472361ab1170805975e775b8b1d
Relm-Arrowny <Relm-Arrowny@users.noreply.github.com>
2025-01-22 10:38:17 +0000

    deploy: 20c161a0bdc3731f3bcb1df40c38b49ee24748f7

--- 40-open-up-slits-and-remove-everything-that-is-in-the-way-of-the-beam-to-detectormove-sample-out-of-the-way-and-pin-to-0-in-sample-stage-x-y-z/_static/documentation_options.js
+++ 40-open-up-slits-and-remove-everything-that-is-in-the-way-of-the-beam-to-detectormove-sample-out-of-the-way-and-pin-to-0-in-sample-stage-x-y-z/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.0.5.dev36+g3992c09',
+    VERSION: '0.0.0.5.dev38+g20c161a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
